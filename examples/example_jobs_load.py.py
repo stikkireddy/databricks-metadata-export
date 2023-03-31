7454452b66d643e7ed845c576b9ba84cd79ac011
stikkireddy <sri.tikkireddy@databricks.com>
2023-03-31 11:41:18 -0400

    remove unneeded commented code

--- examples/example_jobs_load.py.py
+++ examples/example_jobs_load.py.py
@@ -3,11 +3,6 @@ import pyspark
 from databricks_export.jobs import JobRunsHandler
 from delta import *
 
-# builder = pyspark.sql.SparkSession.builder.appName("test") \
-#     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-#     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-
-# spark = configure_spark_with_delta_pip(builder).getOrCreate()
 JobRunsHandler(spark, target_table_location="dbfs:/tmp/sri/jobs_delta_dump_v2",
                 host="", token="", 
                 buffer_size=1000, workspace_name="") \
